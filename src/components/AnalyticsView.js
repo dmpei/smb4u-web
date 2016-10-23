12c5e3aad01a7efde57fc4bcaba2818430694b7d
Dee Pei <deepei@macbooks-MacBook-Pro.local>
2016-10-23 11:04:21 -0700

    Created mock for analytics view

--- src/components/AnalyticsView.js
+++ src/components/AnalyticsView.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const AnalyticsView = () => (
-  <div>
-    <h1>Analytics</h1>
+  <div className="analytics">
+    <img src="https://rawgit.com/dmpei/bedb9b8cab956ae4fb599ed82452204a/raw/759a7c24ab5658c8a51c2e5709a961ada6dded52/sales.svg"></img>
   </div>
 );
 
